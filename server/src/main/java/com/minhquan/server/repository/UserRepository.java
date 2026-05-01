ec2b869a7d14a93408265f03de170ab254567c54
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2026-05-01 13:10:18 +0700

    Add Docker support with Dockerfile and docker-compose.yml; update application.yaml for database configuration

--- server/src/main/java/com/minhquan/server/repository/UserRepository.java
+++ server/src/main/java/com/minhquan/server/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository   <User, Integer> {
     // Hàm tùy chỉnh để tìm User theo tên đăng nhập
     Optional<User> findByUsername(String username);
 }
